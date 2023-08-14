4e1c2f90ecb0df8328429493d3f1b6213f45312a
Suryank Tiwari <suryankt@amazon.com>
2023-08-15 02:11:20 +0530

    Added homepage UI and navigation to blank activities
    [Description]: Added a basic layout using compose for the home page. Added two dummy activities and have added navigation from home page to them upon clicks. Planning to add two modes - quick feed and meal planner to the app at the start.
    
    [Testing]: Verified UI and navigation on simulator

---
+++ app/src/main/java/com/example/birdfeeder/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.birdfeeder
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birdfeeder.ui.theme.BirdFeederTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirdFeederTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ParentFeeder()
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalUnitApi::class)
+    @Composable
+    fun FeederGreeting() {
+        Text(
+            text = "Bird Feeder",
+            fontSize = TextUnit(10f, TextUnitType.Em),
+        )
+    }
+
+    @Composable
+    fun ParentFeeder() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(50.dp)
+        ) {
+            FeederGreeting()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top)
+            ) {
+                    QuickFeeder()
+                    MealPlanner()
+            }
+        }
+
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun QuickFeeder() {
+        Card(
+            onClick = {quickFeed()},
+            modifier = Modifier.size(width = 400.dp, height = 150.dp),
+            shape = CardDefaults.shape,
+            colors = CardDefaults.cardColors(),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            ),
+        ){
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.quick_meal),
+                    contentDescription = null, // decorative
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .fillMaxWidth()
+                )
+
+                Text(
+                    text = "Quick feed",
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(5.dp, 0.dp)
+                )
+
+                Text(
+                    text = "Random one-off meal suggestion",
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(5.dp, 0.dp)
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MealPlanner() {
+        Card(
+            onClick = {planMeal()},
+            modifier = Modifier.size(width = 400.dp, height = 150.dp),
+            shape = CardDefaults.shape,
+            colors = CardDefaults.cardColors(),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            ),
+        ){
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.avocado),
+                    contentDescription = null, // decorative
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .fillMaxWidth()
+                )
+
+                Text(
+                    text = "Meal planner",
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(5.dp, 0.dp)
+                )
+
+                Text(
+                    text = "Plan an entire day's meal",
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(5.dp, 0.dp)
+                )
+            }
+        }
+    }
+
+    private fun quickFeed() {
+        val intent = Intent(this, QuickFeeder::class.java)
+        startActivity(intent)
+    }
+
+    private fun planMeal() {
+        val intent = Intent(this, MealPlanner::class.java)
+        startActivity(intent)
+    }
+
+}
---
+++ app/src/main/java/com/example/birdfeeder/QuickFeeder.kt
@@ -0,0 +1,40 @@
+package com.example.birdfeeder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.birdfeeder.ui.theme.BirdFeederTheme
+
+class QuickFeeder : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirdFeederTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        FeederGreeting()
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun FeederGreeting() {
+        Text("Bird Feeder")
+    }
+}
